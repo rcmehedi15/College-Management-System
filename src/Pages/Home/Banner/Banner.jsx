b6bd153b6320fcbda2275fd626c4c03831ca901a
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-07-23 14:41:40 +0600

    banner done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Banner/Banner.css'
+import Infra from '../../../assets/infra.jpg'
+import InfraNight from '../../../assets/infraNight.jpg'
 
 const Banner = () => {
     return (
         <>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://i.ibb.co/YWzjfsf/bgimg.png"/>
+                    <img src={Infra} className='w-80 rounded-lg rotate-3	'/>
+                    <img src={InfraNight} className='w-80 rounded-lg rotate-3	'/>
                     <div>
-                        <h1 className="text-5xl font-bold text-[#FF42A5] mb-2">Best Robotics Toy Store </h1> <span className="text-4xl font-bold text-[#272626] " >Online Shop!</span>
-                        <p className="py-6">Give The Gift Your Robotics Lover Friends or Family</p>
+                        <h1 className="text-5xl font-bold text-[#FF42A5] mb-2">Infra Polytechnic Institute</h1> <span className="text-4xl font-bold text-[#272626] " >Kasipur,Barishal</span>
+                        <p className="py-6">IPI is committed to providing a dynamic and interesting learning experience with small class sizes and affordable high-quality programs</p>
                         <Link to="/profile" className="btn bg-[#FF42A5] text-white border-none">Get Started </Link> 
                     </div>
                 </div>
